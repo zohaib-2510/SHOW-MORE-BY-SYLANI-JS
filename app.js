df25b661a80bcea8426a40b25c2365d941cbff77
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-08-31 12:53:16 -0700

    COMPLETE
    COMPLETE

---
+++ app.js
@@ -0,0 +1,4 @@
+function showData(){
+    document.getElementById('abc').innerHTML='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus labore quaerat iste libero vitae, minima soluta? Quia iure ipsam commodi, odio tenetur sapiente, qui eaque autem, natus sunt voluptates repudiandae.'
+    document.getElementById('read').style.display='none'
+}
